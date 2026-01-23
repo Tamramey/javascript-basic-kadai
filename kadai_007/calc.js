d64c57f1532b73f63c82938ee6f05fbed455f9ed
Tamramey <tam-ramey@ezweb.ne.jp>
2026-01-24 05:22:58 +0900

    二回目の3と5の倍数

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,12 +1,19 @@
 let num = 15; 
 
 // numが3の倍数なら'3の倍数です'と出力する
-if (num % 3 === 0)
+
 console.log('3の倍数です')
 // numが5の倍数なら'5の倍数です'と出力する
-if (num % 5 === 0)
+
 console.log('5の倍数です')
 // numが’3と5の倍数です’と出力する
-if ((num % 3 && mum % 5 ) ==0) {
-  console.log ('3と5の倍数です')
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です') 
+}
+else if(num % 3 === 0) {
+  console.log
+}
+
+else if(num % 5 === 0) {
+  console.log
 }
