994e89ddc36c3a539e5bba8d86ea2f237b42cd2c
Tamramey <tam-ramey@ezweb.ne.jp>
2026-01-24 02:21:30 +0900

    3と5の倍数

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,4 +1,11 @@
-const weight = 68;
+let weight = 68;
 const height = 1.7;
 const bmi = weight / (height * height)
+weight = 0;
 console.log(bmi)
+const num = 5;
+
+// もし定数numが10より大きいならば、「定数numは10より大きいです」という文字列を出力する
+if (num > 10) {
+  console.log('定数numは10より大きいです');
+}
